55f347361863755765caef845bd5fae196e28924
Deepak <deepakworkpc@gmail.com>
2024-07-28 16:07:45 +0530

    feat: serve about, index, contact, 404 page

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let http = require("node:http");
+let fs = require("node:fs");
 
 const PORT = 8000;
 
 http.createServer((req, res) => {
-    console.log(1)
-    res.end("deo")
+    let path = req.url == "/" ? "index" : req.url;
+
+    fs.readFile(`pages/${path}.html`, (err, data) => {
+        if (err) {
+            console.error(err)
+        }
+        res.end(data);
+    })
 }).listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`)
 })
